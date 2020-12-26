66a2fd8d14be09ad1dc35ff0060ea79ee58e98e7
karel <kareldebrouwere@gmail.com>
2020-12-26 16:27:42 +0100

    test
    test

---
+++ kwwwis.py
@@ -0,0 +1,15 @@
+import cherrypy
+import os
+
+class kwwwis(object):
+    @cherrypy.expose
+    def index(self):
+        return "kwwwis!!!!"
+
+
+if __name__ == '__main__':
+    print(os.getcwd())
+    #os.chdir("/home/pi/Documents/")
+    print(os.getcwd())
+    conf = os.path.join(os.path.dirname(__file__),'kwwwis.conf')
+    cherrypy.quickstart(kwwwis(),config=conf)
